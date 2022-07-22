bdbf3e0b9c9f5aa2e81b4a012a46a710a910d4b8
luthfeew <luthfeew@gmail.com>
2022-07-22 08:51:43 +0700

    ui

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pyodide import create_proxy
+
 page_loading = Element("page_loading").element
 
 view_feature = Element("view_feature").element
@@ -6,12 +8,19 @@ view_main = Element("view_main").element
 x_uncensor = Element("x_uncensor").element
 x_artifact = Element("x_artifact").element
 
+goto_feature = Element("goto_feature").element
+breadcrumb1 = Element("breadcrumb1").element
+breadcrumb2 = Element("breadcrumb2").element
 x_input = Element("x_input").element
 x_output = Element("x_output").element
 
 
 def loading_done():
     page_loading.classList.add("is-hidden")
+    show_feature()
+
+
+def goto_feature_click(event):
     show_feature()
 
 
@@ -22,8 +31,22 @@ def show_feature():
     x_output.value = ""
 
 
+def show_main(id):
+    view_main.classList.remove("is-hidden")
+    view_feature.classList.add("is-hidden")
+    breadcrumb1.innerHTML = id.closest(":not(button)").previousElementSibling.innerHTML
+    breadcrumb2.innerHTML = id.innerHTML
+
+
+def x_uncensor_click(event):
+    show_main(x_uncensor)
+    # view_x_uncensor.classList.remove("is-hidden")
+
+
 def main():
     loading_done()
+    goto_feature.addEventListener("click", create_proxy(goto_feature_click))
+    x_uncensor.addEventListener("click", create_proxy(x_uncensor_click))
 
 
 main()
