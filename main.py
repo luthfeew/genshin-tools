6dd4d0f0e2b9f3a2d1dcb4375a701f00de024220
luthfeew <luthfeew@gmail.com>
2022-07-22 11:18:06 +0700

    uncensor done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyodide import create_proxy
+from js import document
 
 page_loading = Element("page_loading").element
 
@@ -8,11 +9,46 @@ view_main = Element("view_main").element
 x_uncensor = Element("x_uncensor").element
 x_artifact = Element("x_artifact").element
 
+view_x_uncensor = Element("view_x_uncensor").element
+# x_uncensor_n = Element("x_uncensor_n").element
+# x_uncensor_ext = Element("x_uncensor_ext").element
+
 goto_feature = Element("goto_feature").element
 breadcrumb1 = Element("breadcrumb1").element
 breadcrumb2 = Element("breadcrumb2").element
 x_input = Element("x_input").element
-x_output = Element("x_output").element
+# x_output = Element("x_output").element
+proc_copy = Element("proc_copy").element
+
+uncensor_dict = {
+    "A": "А",
+    "a": "а",
+    "B": "В",
+    "E": "Е",
+    "e": "е",
+    "K": "К",
+    "M": "М",
+    "H": "Н",
+    "O": "О",
+    "o": "о",
+    "P": "Р",
+    "p": "р",
+    "C": "С",
+    "c": "с",
+    "T": "Т",
+    "y": "у",
+    "X": "Х",
+    "x": "х",
+    "I": "І",
+    "i": "і",
+}
+
+# uncensor_dict_ext = {
+#     "r": "г",
+#     "b": "Ь",
+#     "V": "Ѵ",
+#     "v": "ѵ",
+# }
 
 
 def loading_done():
@@ -28,7 +64,8 @@ def show_feature():
     view_feature.classList.remove("is-hidden")
     view_main.classList.add("is-hidden")
     x_input.value = ""
-    x_output.value = ""
+    # x_output.value = ""
+    view_x_uncensor.classList.add("is-hidden")
 
 
 def show_main(id):
@@ -40,13 +77,21 @@ def show_main(id):
 
 def x_uncensor_click(event):
     show_main(x_uncensor)
-    # view_x_uncensor.classList.remove("is-hidden")
+    view_x_uncensor.classList.remove("is-hidden")
+
+
+def proc_copy_click(event):
+    for key, value in uncensor_dict.items():
+        x_input.value = x_input.value.replace(key, value)
+    x_input.select()
+    document.execCommand("copy")
 
 
 def main():
     loading_done()
     goto_feature.addEventListener("click", create_proxy(goto_feature_click))
     x_uncensor.addEventListener("click", create_proxy(x_uncensor_click))
+    proc_copy.addEventListener("click", create_proxy(proc_copy_click))
 
 
 main()
